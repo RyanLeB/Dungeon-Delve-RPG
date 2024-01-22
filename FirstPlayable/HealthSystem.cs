905796154694b964125f738a2a0604c5fc4debe8
Ryan LeBlanc <ryanleb2003@gmail.com>
2024-01-22 12:37:49 -0400

    Some classes created

---
+++ FirstPlayable/HealthSystem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstPlayable
+{
+    internal class HealthSystem
+    {
+
+        public int health;
+        
+        public void Heal(int health)
+        {
+
+        }
+
+        public void TakeDamage(int health) 
+        {
+        
+        }
+    
+    }
+}
