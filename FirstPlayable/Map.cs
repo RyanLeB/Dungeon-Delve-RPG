946a59081318a052e4e37bf2d4b845a19977ebf3
Ryan LeBlanc <ryanleb2003@gmail.com>
2024-01-30 17:03:18 -0400

    Map class change

--- FirstPlayable/Map.cs
+++ FirstPlayable/Map.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.SqlServer.Server;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,14 @@ namespace FirstPlayable
 {
     internal class Map
     {
-        
-    
+        public static string path;
+        public static string RPGMap = @"RPGMap.txt";
+        static string[] floor;
+        static char[,] layout;
+
+        static int mapX;
+        static int mapY;
+        static int maximumX;
+        static int maximumY;
     }
 }
