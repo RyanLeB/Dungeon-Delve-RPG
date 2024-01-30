66c85a63df6568445bb4f79efdcc251d5bd3f26d
Ryan LeBlanc <ryanleb2003@gmail.com>
2024-01-30 18:16:25 -0400

    Redone health system and map class

--- FirstPlayable/HealthSystem.cs
+++ FirstPlayable/HealthSystem.cs
@@ -11,14 +11,34 @@ namespace FirstPlayable
 
         public int health;
         
-        public void Heal(int health)
+
+        public HealthSystem()
         {
 
         }
 
-        public void TakeDamage(int health) 
+        public void TakeDamage(int damage)
         {
+            health -= damage;
+            if (health <= 0) 
+            {
+                health = 0;
+            }
+        }
+
+        public void Heal(int recoverHP, int maxHealth)
+        {
+            health += recoverHP;
+            if (health > maxHealth)
+            {
+                health = maxHealth;
+            }
+        }
         
+        
+        public void SetHealth(int maxHP) 
+        {
+            health = maxHP;
         }
     
     }
--- FirstPlayable/Map.cs
+++ FirstPlayable/Map.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.SqlServer.Server;
-using System;
+﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,14 +9,58 @@ namespace FirstPlayable
 {
     internal class Map
     {
-        public static string path;
-        public static string RPGMap = @"RPGMap.txt";
+        int mapX;
+        int mapY;
+        
+        public string mapPath;
+        string level1 = @"RPGMap1.txt";
+        string level2 = @"RPGMap2.txt";
         static string[] floor;
         static char[,] layout;
 
-        public int mapX;
-        static int mapY;
-        static int maximumX;
-        static int maximumY;
+        // Constructor
+        public Map()
+        {
+            LoadMap();
+        }
+
+        // Loads level map
+        public void LoadMap()
+        {
+            mapPath = level1;
+            floor = File.ReadAllLines(mapPath);
+            layout = new char[floor.Length, floor[0].Length];
+            MakeMap();
+        }
+
+        // Makes level map
+        public void MakeMap()
+        {
+            for (int i = 0; i < floor.Length; i++)
+            {
+                for (int j = 0; j < floor[i].Length; j++)
+                {
+                    layout[i, j] = floor[i][j];
+                }
+            }
+        }
+
+        // Draws level map
+        public void DrawMap(char[,] levelMap) 
+        { 
+            Console.SetCursorPosition(0, 0);
+            for(int y = 0; y < mapY; y++)
+            {
+                for (int x = 0; x < mapX; x++)
+                {
+                    char tile = levelMap[y, x];
+
+                }
+                Console.WriteLine("\n");
+            }
+            Console.SetCursorPosition(0, 0);
+        }
+
+
     }
 }
